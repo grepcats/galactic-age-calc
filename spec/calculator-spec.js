753e8e7a63023d9475b3e30def5ab11e24001dd1
Kayla Ondracek <kayla.renee@gmail.com>
2018-03-16 10:22:33 -0700

    add red test for converting users birthday to seconds

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -7,4 +7,12 @@ describe('Calculator', function() {
     expect(calc.name).toEqual("Ben");
     expect(calc.birthday).toEqual("4/19/1986");
   });
+
+  it('converts users age to seconds', function() {
+    let calc = new Calculator("Ben", "4/19/1986");
+    calc.convertToSeconds();
+    let seconds = 514278000;
+
+    expect(calc.convertedBirthday.toEqual(seconds));
+  });
 });
--- src/calculator.js
+++ src/calculator.js
@@ -2,5 +2,12 @@ export class Calculator {
   constructor(name, birthday) {
     this.name = name;
     this.birthday = birthday;
+    this.convertedBirthday = new Date();
   }
+
+  convertToSeconds() {
+    //converts birthday to seconds and sets it
+  }
+
+
 }
