afb7bc065d1b033091c6ce7f9155035671d73938
Kayla Ondracek <kayla.renee@gmail.com>
2018-03-16 16:35:24 -0700

    add more tests for life expectancy given longer life

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -2,11 +2,14 @@ import { Calculator } from '../src/calculator.js';
 
 describe('Calculator', function() {
   let calc;
+  let older;
   const yearCalc = 86400;
 
   beforeEach(function() {
     calc = new Calculator("Ben", "4/19/1986", "male");
+    older = new Calculator("Bill", "1/1/1930", "male");
     calc.convertToSeconds();
+    older.convertToSeconds();
   });
 
   it('creates a calculator object with the users name and birthday', function() {
@@ -57,42 +60,63 @@ describe('Calculator', function() {
   });
 
   it('provides life expectancy for earth', function() {
-    let expectency = 76.3
+    let expectency = 76.3;
     let result = "Your life expectancy is " + Number(expectency - calc.planetAgeConvert("earth")).toFixed(1) + " more years.";
     expect(calc.lifeExpectancy("earth")).toEqual(result);
   });
 
   it('provides life expectancy for mercury', function() {
-    let expectency = 76.3 / .24
+    let expectency = 76.3 / .24;
     let result = "Your life expectancy is " + Number(expectency - calc.planetAgeConvert("mercury")).toFixed(1) + " more years.";
     expect(calc.lifeExpectancy("mercury")).toEqual(result);
   });
 
   it('provides life expectancy for venus', function() {
-    let expectency = 76.3 / .62
+    let expectency = 76.3 / .62;
     let result = "Your life expectancy is " + Number(expectency - calc.planetAgeConvert("venus")).toFixed(1) + " more years.";
     expect(calc.lifeExpectancy("venus")).toEqual(result);
   });
 
   it('provides life expectancy for mars', function() {
-    let expectency = 76.3 / 1.88
+    let expectency = 76.3 / 1.88;
     let result = "Your life expectancy is " + Number(expectency - calc.planetAgeConvert("mars")).toFixed(1) + " more years.";
     expect(calc.lifeExpectancy("mars")).toEqual(result);
   });
 
   it('provides life expectancy for jupiter', function() {
-    let expectency = 76.3 / 11.86
+    let expectency = 76.3 / 11.86;
     let result = "Your life expectancy is " + Number(expectency - calc.planetAgeConvert("jupiter")).toFixed(1) + " more years.";
     expect(calc.lifeExpectancy("jupiter")).toEqual(result);
   });
 
-  it('tells user how many years theyve passed life expectancy if they have', function() {
-    let older = new Calculator("Bill", "1/1/1930", "male");
-    older.convertToSeconds();
-    let venusExpectancy = 76.3 / .62
-    let venusAge = older.planetAgeConvert("venus");
-    let result = "You've outlived your life expectancy by " + Number(venusAge - venusExpectancy).toFixed(1) + " years.";
+  it('tells user how many years theyve passed life expectancy for earth', function() {
+    let expectancy = 76.3;
+    let result = "You've outlived your life expectancy by " + Number(older.planetAgeConvert("earth") - expectancy).toFixed(1) + " years.";
+    expect(older.lifeExpectancy("earth")).toEqual(result);
+  });
+
+  it('tells user how many years theyve passed life expectancy for mercury', function() {
+    let expectancy = 76.3 / .24;
+    let result = "You've outlived your life expectancy by " + Number(older.planetAgeConvert("mercury") - expectancy).toFixed(1) + " years.";
+
+    expect(older.lifeExpectancy("mercury")).toEqual(result);
+  });
+
+  it('tells user how many years theyve passed life expectancy for venus', function() {
+    let expectancy = 76.3 / .62;
+    let result = "You've outlived your life expectancy by " + Number(older.planetAgeConvert("venus") - expectancy).toFixed(1) + " years.";
     expect(older.lifeExpectancy("venus")).toEqual(result);
   });
 
+  it('tells user how many years theyve passed life expectancy for mars', function() {
+    let expectancy = 76.3 / 1.88;
+    let result = "You've outlived your life expectancy by " + Number(older.planetAgeConvert("mars") - expectancy).toFixed(1) + " years.";
+    expect(older.lifeExpectancy("mars")).toEqual(result);
+  });
+
+  it('tells user how many years theyve passed life expectancy for jupiter', function() {
+    let expectancy = 76.3 / 11.86;
+    let result = "You've outlived your life expectancy by " + Number(older.planetAgeConvert("jupiter") - expectancy).toFixed(1) + " years.";
+    expect(older.lifeExpectancy("jupiter")).toEqual(result);
+  });
 });
